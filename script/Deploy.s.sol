0d034fe5ceea0b17b6661b3ad011515c353037c4
labsmula <labsmula@gmail.com>
2026-06-02 16:46:33 +0800

    feat: deploy full contract stack

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
 import {AgentIdentity} from "../contracts/AgentIdentity.sol";
+import {AutopilotPolicy} from "../contracts/AutopilotPolicy.sol";
 import {DecisionLog} from "../contracts/DecisionLog.sol";
+import {ReputationRegistry} from "../contracts/ReputationRegistry.sol";
 import {RiskPolicy} from "../contracts/RiskPolicy.sol";
+import {ValidationRegistry} from "../contracts/ValidationRegistry.sol";
 
 contract DeployScript is Script {
     function run() external {
@@ -13,11 +16,17 @@ contract DeployScript is Script {
         AgentIdentity agentIdentity = new AgentIdentity();
         DecisionLog decisionLog = new DecisionLog();
         RiskPolicy riskPolicy = new RiskPolicy();
+        ReputationRegistry reputationRegistry = new ReputationRegistry(address(agentIdentity));
+        ValidationRegistry validationRegistry = new ValidationRegistry(address(agentIdentity));
+        AutopilotPolicy autopilotPolicy = new AutopilotPolicy();
 
         vm.stopBroadcast();
 
         console2.log("AgentIdentity:", address(agentIdentity));
         console2.log("DecisionLog:", address(decisionLog));
         console2.log("RiskPolicy:", address(riskPolicy));
+        console2.log("ReputationRegistry:", address(reputationRegistry));
+        console2.log("ValidationRegistry:", address(validationRegistry));
+        console2.log("AutopilotPolicy:", address(autopilotPolicy));
     }
 }
